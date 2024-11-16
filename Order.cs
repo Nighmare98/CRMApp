8a43e4912edf7ba6b74536088e095dc04ccd0233
Nighmare98 <61187443+Nighmare98@users.noreply.github.com>
2024-11-16 18:45:42 +0700

    Create Order.cs

---
+++ Order.cs
@@ -0,0 +1,9 @@
+namespace CRMApp
+{
+    public class Order
+    {
+        public string ClientName { get; set; }
+        public string ServiceName { get; set; }
+        public string Status { get; set; }
+    }
+}
